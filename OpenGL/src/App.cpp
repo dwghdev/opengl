f67af6b136f1807ddb7eeeebe56151a3bc40286b
Walter <dalewaltergh@gmail.com>
2022-02-28 09:02:47 +0800

    display red triangle by writing vertex and fragment shader

--- OpenGL/src/App.cpp
+++ OpenGL/src/App.cpp
@@ -3,6 +3,46 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+  unsigned int id = glCreateShader(type);
+  const char* src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char* message = (char*)alloca(length * sizeof(char));
+    glGetShaderInfoLog(id, length, &length, message);
+    std::cout << "Failed to compile" 
+      << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") 
+        << "shader!" << std::endl;
+    std::cout << message << std::endl;
+    glDeleteShader(id);
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+  unsigned int program = glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
+
 int main() {
   GLFWwindow* window;
 
@@ -37,7 +77,29 @@ int main() {
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
-  glVertextAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); 
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); 
+
+  std::string vertexShader = R"glsl(
+    #version 330 core
+
+    layout(location = 0) in vec4 position;
+
+    void main() {
+      gl_Position = position;
+    }
+  )glsl";
+
+  std::string fragmentShader = R"glsl(
+    #version 330 core
+
+    layout(location = 0) out vec4 color;
+
+    void main() {
+      color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  )glsl";
+  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  glUseProgram(shader);
 
   // Loop until user closes the window
   while (!glfwWindowShouldClose(window)) {
@@ -52,6 +114,7 @@ int main() {
     // Poll for and process events 
     glfwPollEvents();
   }
+  glDeleteProgram(shader);
 
   glfwTerminate();
   return 0;
